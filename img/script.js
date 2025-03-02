f52dc2ea18913c6137fe07fe02a28432b877e8e4
Mickyreegs <w58fyus5@students.codeinstitute.net>
2025-03-02 10:30:13 +0000

    Add events code and challenge

--- img/script.js
+++ img/script.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
-    $("#stream1_btn").on("click", function() {
+    $("#stream1_btn").click(function() { //Acts the same as the below on clicks, just shorter code
+     //other uses might be .mouseenter or .mouseleave
         $(".stream1").removeClass('highlight_stream');
         $(".stream2").removeClass('highlight_stream');
         $(".stream3").removeClass('highlight_stream');
@@ -18,3 +19,24 @@ $(document).ready(function() {
         $(".stream3").addClass('highlight_stream');
    });
 }); 
+
+/*
+These are examples from the challenge:
+$(document).ready(function() {
+    $("p").click(function() {
+       $("p").css("color","red"); 
+    });
+    
+    $("h2").hover(function() {
+       $("h2").css("background","lightblue"); 
+    });
+    
+    $(".card-panel").mouseenter(function() {
+       $("body").css("background-color","black");
+    });
+    
+    $(".card-panel").mouseleave(function() {
+       $("body").css("background-color","#e1e2e2");
+    });
+});
+*/
