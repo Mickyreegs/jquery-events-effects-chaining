5c58ad7a04388144ac87bca46e42ce948d5f8d68
Mickyreegs <w58fyus5@students.codeinstitute.net>
2025-03-02 12:29:06 +0000

    Add this challenge part 2

--- img/script.js
+++ img/script.js
@@ -110,3 +110,32 @@ $(document).ready(function() {
     });
 });
  */
+
+
+//-----------------This Challenge 1
+/*
+$(document).ready(function() {
+    
+    $("p").click(function() {
+        $(this).text("Changed only the text for this paragraph");
+    });
+    
+    $(".card").mouseenter(function() {
+        $(this).fadeTo(2000, 0.2).fadeTo(2000,1);
+    });
+    
+});
+*/
+
+//-----------------This Challenge 1
+/*
+$(".module-nav").on("click", function() {
+    let allModulesCardsSelector = ".card";
+    let thisModuleCardsSelector  = "." + this.id + "-card";
+
+    $(allModulesCardsSelector).removeClass("card-highlight");
+    
+    $(thisModuleCardsSelector).addClass("card-highlight");
+    
+});
+*/
